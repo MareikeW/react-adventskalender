510edf6f718a7a4f4bb8b706df876ba9eb1fe7a4
Mareike Wendler <49586574+MareikeW@users.noreply.github.com>
2021-11-17 20:43:06 +0100

    24 Türchen mit Zahlen in ungeordneter Reihenfolge angelegt

--- src/App.js
+++ src/App.js
@@ -1,8 +1,59 @@
+import { useState, useEffect } from 'react';
 import './App.css';
+import Tuerchen from "./Tuerchen";
 
 function App() {
+  const [tage, setTage] = useState([]);
+  
+  //let randomTag = Math.floor(Math.random() * 24) + 1;
+  // Erstellt 24 Felder, die nicht geöffnet sind.
+  useEffect(() => {
+    for (let i = 1; i <= 24; i++) {
+        setTage(prevTage => {
+            return [
+                ...prevTage,
+                {
+                    id: i,
+                    selected: false
+                }
+            ]
+        })
+    }
+  }, []);
+  
+  
+  /*const handleClick = ziffer => {
+    tage.map(tag => {
+      if (tag.id === ziffer) {
+          tag.selected ? tag.selected = false : tag.selected = true                   
+      }
+      return tag;        
+    })  
+  }*/
+
+  function createTuerchen() {
+    const alleTuerchen = tage.map(tag => <Tuerchen key={tag.id} tag={tag.id} />);
+
+    shuffleArray(alleTuerchen);
+
+    return alleTuerchen;
+  }
+  
+  function shuffleArray(alleTuerchen) {
+    for (let i = 0; i < alleTuerchen.length - 1; i++) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [alleTuerchen[i], alleTuerchen[j]] = [alleTuerchen[j], alleTuerchen[i]];
+    }
+    return alleTuerchen;
+  }
+
   return (
-    <h1>Adventskalender</h1>
+    <div>
+      <h1>Adventskalender</h1>
+      <div className="kalender">
+        {createTuerchen()}
+      </div>
+    </div>
   );
 }
 
---
+++ src/Tuerchen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import './App.css';
+
+const Tuerchen = ({ tag }) => {
+
+
+    return (
+        <div className="tuerchen">{tag}</div>
+    )
+}
+
+export default Tuerchen;
